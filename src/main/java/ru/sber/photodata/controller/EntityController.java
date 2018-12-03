ce7a5d333a4f0a45c50cbb32aeae7095d88ecb17
Andrew G <andrewmessage@mail.ru>
2018-12-03 12:41:01 +0300

    03122018

--- src/main/java/ru/sber/photodata/controller/EntityController.java
+++ src/main/java/ru/sber/photodata/controller/EntityController.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public interface EntityController <T extends BaseEntity> {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    T create(@RequestBody T someEntity);
+    T create( T someEntity);
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    T update(@RequestBody T someEntity);
+    T update( T someEntity);
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     T get(@PathVariable("id") long id);
